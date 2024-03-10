06c7310ba73fb0ffa6335599462aa1822eb4a436
sqhuang0103 <bitsqhuang@gmail.com>
2024-03-10 20:14:41 +0000

    demo implementation

--- scratch.py
+++ scratch.py
@@ -24,27 +24,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import gc
 
-def show_mask(mask, ax, random_color=False):
-    if random_color:
-        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
-    else:
-        color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
-    h, w = mask.shape[-2:]
-    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
-    ax.imshow(mask_image)
-    del mask
-    gc.collect()
-
-def show_masks_on_image(raw_image, masks):
-  plt.imshow(np.array(raw_image))
-  ax = plt.gca()
-  ax.set_autoscale_on(False)
-  for mask in masks:
-      show_mask(mask, ax=ax, random_color=True)
-  plt.axis("off")
-  plt.show()
-  del mask
-  gc.collect()
+# def show_mask(mask, ax, random_color=False):
+#     if random_color:
+#         color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+#     else:
+#         color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
+#     h, w = mask.shape[-2:]
+#     mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+#     ax.imshow(mask_image)
+#     del mask
+#     gc.collect()
+#
+# def show_masks_on_image(raw_image, masks):
+#   plt.imshow(np.array(raw_image))
+#   ax = plt.gca()
+#   ax.set_autoscale_on(False)
+#   for mask in masks:
+#       show_mask(mask, ax=ax, random_color=True)
+#   plt.axis("off")
+#   plt.show()
+#   del mask
+#   gc.collect()
 
 from transformers import pipeline
 generator = pipeline("mask-generation", model="facebook/sam-vit-huge", device=0)
@@ -54,10 +54,12 @@ import requests
 img_url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/transformers/tasks/car.jpg"
 raw_image = Image.open(requests.get(img_url, stream=True).raw).convert("RGB")
 
-plt.imshow(raw_image)
+# plt.imshow(raw_image)
 
 outputs = generator(raw_image, points_per_batch=64)
 masks = outputs["masks"]
-show_masks_on_image(raw_image, masks)
+# show_masks_on_image(raw_image, masks)
 
-plt.show()
+# plt.show()
+import pdb
+pdb.set_trace()
