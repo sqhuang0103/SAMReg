acfe85f4120211fe0d91985143c7b5580284c304
sqhuang0103 <bitsqhuang@gmail.com>
2024-03-19 16:19:23 +0000

    demo implementation

--- scratch.py
+++ scratch.py
@@ -26,20 +26,61 @@ import gc
 
 
 
+# from transformers import pipeline
+# generator = pipeline("mask-generation", model="facebook/sam-vit-huge", device='cuda:1')
+# from PIL import Image
+# import requests
+#
+# img_url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/transformers/tasks/car.jpg"
+# raw_image = Image.open(requests.get(img_url, stream=True).raw).convert("RGB")
+#
+# # plt.imshow(raw_image)
+#
+# outputs = generator(raw_image, points_per_batch=64)
+# masks = outputs["masks"] #len 59, masks[0] (480,640)
+# # show_masks_on_image(raw_image, masks)
+#
+# # plt.show()
+import cv2
+import PIL
+import numpy as np
+def visualize_masks(binary_masks, pil_image):
+    # Convert PIL image to numpy array
+    background = np.array(pil_image)
+
+    # Convert RGB to BGR (OpenCV uses BGR color format)
+    background = cv2.cvtColor(background, cv2.COLOR_RGB2BGR)
+
+    # Create a blank mask
+    mask = np.zeros_like(background)
+
+    # Define colors for different masks
+    colors = [(0, 255, 0), (0, 0, 255), (255, 0, 0)]  # You can add more colors as needed
+
+    # Iterate through binary masks and overlay on the blank mask with different colors
+    for idx, binary_mask in enumerate(binary_masks):
+        # Convert binary mask to uint8
+        binary_mask = np.uint8(binary_mask)
+
+        # Create a mask where binary mask is True
+        fg_mask = np.where(binary_mask, 255, 0).astype(np.uint8)
+
+        # Apply the foreground mask on the result with the corresponding color
+        mask[fg_mask > 0] = colors[idx % len(colors)]
+
+    # Add the mask on top of the background image
+    result = cv2.addWeighted(background, 1, mask, 0.5, 0)
+
+    return result
+
+im1 = Image.open("/raid/shiqi/slice_1_3.png").convert("RGB")
+im2 = Image.open("/raid/shiqi/slice_1_1.png").convert("RGB")
+device='cuda:0'
 from transformers import pipeline
-generator = pipeline("mask-generation", model="facebook/sam-vit-huge", device='cuda:1')
-from PIL import Image
-import requests
-
-img_url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/transformers/tasks/car.jpg"
-raw_image = Image.open(requests.get(img_url, stream=True).raw).convert("RGB")
-
-# plt.imshow(raw_image)
-
-outputs = generator(raw_image, points_per_batch=64)
-masks = outputs["masks"] #len 59, masks[0] (480,640)
-# show_masks_on_image(raw_image, masks)
-
-# plt.show()
-
-
+generator = pipeline("mask-generation", model="facebook/sam-vit-huge", device='cuda:0')
+outputs = generator(im1, points_per_batch=64)
+masks = outputs["masks"]
+visualized_image = visualize_masks(masks, im1)
+cv2.imshow("Visualized Image", visualized_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
