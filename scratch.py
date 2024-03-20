6474e20981180591f522021117e28532a1f28dfe
sqhuang0103 <bitsqhuang@gmail.com>
2024-03-20 14:59:37 +0000

    demo implementation

--- scratch.py
+++ scratch.py
@@ -132,8 +132,8 @@ masks = _mask_criteria(masks)
 
 from model.segment_anything import SamAutomaticMaskGenerator, sam_model_registry, SamPredictor
 # im1_cv = cv2.imread("/raid/candi/shiqi/slice_1_3.png")
-im1_cv = cv2.imread("/home/shiqi/SAMReg/example/cell/PNT1A_do_1_f00_01_01_R.png")
-im2_cv = cv2.imread("/home/shiqi/SAMReg/example/cell/PNT1A_do_1_f00_01_01_T.png")
+im1_cv = cv2.imread("/home/shiqi/SAMReg/example/cell/PNT1A_do_1_f00_01_01_R.png",0)
+im2_cv = cv2.imread("/home/shiqi/SAMReg/example/cell/PNT1A_do_1_f00_01_01_T.png",0)
 # im2_cv = cv2.imread("/raid/candi/shiqi/slice_1_1.png")
 sam = sam_model_registry['vit_h'](checkpoint='/raid/candi/shiqi/sam_pretrained/sam_vit_h_4b8939.pth')
 sam.to(device=device)
