cf9738849eef5c8d3ed1108bc13252a453375060
sqhuang0103 <bitsqhuang@gmail.com>
2024-04-14 14:56:35 +0100

    demo implementation

--- roi_match_3d.py
+++ roi_match_3d.py
@@ -11,4 +11,25 @@ import torch.nn.functional as F
 import random
 device = 'cpu'
 
-def
+class ROIMatching():
+    def __init__(self):
+        def __init__(self, img1, img2, device='cuda:1', v_min=200, v_max=7000, mode='embedding',
+                     url="facebook/sam-vit-huge", sim_criteria=0.8):
+            """
+            Initialize
+            :param img1: PIL image
+            :param img2:
+            """
+            self.img1 = img1
+            self.img2 = img2
+            self.device = device
+            self.v_min = v_min
+            self.v_max = v_max
+            self.mode = mode
+            self.url = url
+            self.sim_criteria = sim_criteria
+
+        def _get_model(self):
+            pass
+        def _get_prompt_mask(self):
+            pass
