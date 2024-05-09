421ab1e5281810aee6fbe11b569dcade37e7ec27
sqhuang0103 <bitsqhuang@gmail.com>
2024-05-09 15:41:28 +0100

    demo implementation

--- roi_match.py
+++ roi_match.py
@@ -312,7 +312,7 @@ class RoiMatching():
             cv2.imwrite('/home/shiqi/ori_mov_roi.png', hm_ori_soft_mov_roi)
 
             ### visualize prompts
-            mask_prompt = np.uint8(ori_soft_mov_roi>0.9)
+            mask_prompt = np.uint8(ori_soft_mov_roi>0.87)
             point_prompt = np.uint8(ori_soft_mov_roi>= ori_soft_mov_roi.max())
 
             # draw point prompt
