94e0a10f07287b3cf42d9aab3bd709e3648c127a
sqhuang0103 <bitsqhuang@gmail.com>
2024-05-10 17:38:00 +0100

    demo implementation

---
+++ draw_seaborn.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+sns.set_theme(style="darkgrid")
+# Load an example dataset with long-form data
+fmri = sns.load_dataset("fmri")
+
+# Plot the responses for different events and regions
+sns.lineplot(x="timepoint", y="signal",
+             hue="region", style="event",
+             data=fmri)
